26d23f7cdff7636a92504844111495e2c9531526
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-06-27 23:55:47 +0100

    validate_utf function

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """ Check if data represents a valid UTF-8 encoding. """
+    n_bytes = 0
+    
+    for i in data:
+        if n_bytes > 0:
+            if not (i >> 6 == 2):
+                return False
+            n_bytes -= 1
+        else:
+            if i >> 7 == 0:
+                n_bytes = 0
+            elif i >> 5 == 6:
+                n_bytes = 1
+            elif i >> 4 == 14:
+                n_bytes = 2
+            elif i >> 3 == 30:
+                n_bytes = 3
+            else:
+                return False
+    # If n_bytes is not zero, it means there are trailing bytes missing
+    return n_bytes == 0
