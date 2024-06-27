722435c5442a67f53f5494f81e6a20133e53cb08
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-06-28 00:06:33 +0100

    fix some errors

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,20 +7,22 @@ from typing import List
 def validUTF8(data: List[int]) -> bool:
     """ Check if data represents a valid UTF-8 encoding. """
     n_bytes = 0
-    
+
     for i in data:
+        i = i & 0xFF  # 10010110 & 11111111 results in 10010110 (which is 150)
+
         if n_bytes > 0:
-            if not (i >> 6 == 2):
+            if (i >> 6) != 2:
                 return False
             n_bytes -= 1
         else:
-            if i >> 7 == 0:
+            if (i >> 7) == 0:
                 n_bytes = 0
-            elif i >> 5 == 6:
+            elif (i >> 5) == 6:
                 n_bytes = 1
-            elif i >> 4 == 14:
+            elif (i >> 4) == 14:
                 n_bytes = 2
-            elif i >> 3 == 30:
+            elif (i >> 3) == 30:
                 n_bytes = 3
             else:
                 return False
