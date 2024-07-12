e6f24bbf3fe6ebfea9363841484e87fba0b04069
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-12 01:52:01 +0100

    fix some issues

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,23 @@
-#!/usr/bin/node
+#!/usr//bin/node
 const request = require('request');
 
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url + movieId, (err, response, body) => {
+request(url, (err, response, body) => {
   if (err) throw err;
   const actor = JSON.parse(body).characters;
   printName(actor);
 });
 
 const printName = (names, i = 0) => {
-  if (i === names.length) return;
+  if (i === names.length) {
+    return;
+  }
   request(names[i], (err, response, body) => {
-    if (err) throw err;
+    if (err) {
+        throw err;
+    }
     const character = JSON.parse(body);
     console.log(character.name);
     printName(names, i + 1);
